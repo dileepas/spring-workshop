539d103b195c4b09b451f7f4e5639aeae5ae5383
Dileepa Sandaruwan <dileepa.sandaruwan@pagero.com>
2024-08-04 16:41:20 +0200

    Update response type

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.service.HttpService;
+import com.example.demo.types.Departure;
 import com.example.demo.types.DeparturesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -24,14 +27,14 @@ public class DemoApplication {
 		}
 
 		@GetMapping("/ping")
-		public DeparturesResponse ping() throws Exception {
+		public List<Departure> ping() throws Exception {
 /*            String url = "https://transport.integration.sl.se/v1/sites/4305/departures?transport=BUS&line=474&direction=2";
 			HttpRequest req = HttpRequest.newBuilder().uri(new URI(url)).GET().build();
 			HttpResponse<String> response = HttpClient.newHttpClient().send(req, BodyHandlers.ofString());
 			String res = response.body();
 			ObjectMapper objectMapper = new ObjectMapper();
 			DeparturesResponse  list = objectMapper.readValue(res, DeparturesResponse.class);*/
-			return service.getList();
+			return service.getList().getDepartures();
 		}
 	}
 }
